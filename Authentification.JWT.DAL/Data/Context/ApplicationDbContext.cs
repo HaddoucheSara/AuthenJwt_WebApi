a5a007910190d3c4831c0b958d5d21a8fcff9e51
HaddoucheSara <124178343+HaddoucheSara@users.noreply.github.com>
2025-04-17 18:59:57 +0100

    AppContext fixed

--- Authentification.JWT.DAL/Data/Context/ApplicationDbContext.cs
+++ Authentification.JWT.DAL/Data/Context/ApplicationDbContext.cs
@@ -5,14 +5,19 @@ namespace Authentification.JWT.DAL.Data.Context
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
 
         public DbSet<User> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Server=tcp:mymicroappserver.database.windows.net,1433;Initial Catalog=MyMicroAppDb;Persist Security Info=False;User ID=sara;Password=azureAzure@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=tcp:mymicroappserver.database.windows.net,1433;Initial Catalog=MyMicroAppDb;Persist Security Info=False;User ID=sara;Password=azureAzure@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
         }
     }
 }
